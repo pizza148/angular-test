67a4c6978ce1d641d22702365bb3e631e1926aba
Aung Kaung Khant <aungkaungkhant807@gmail.com>
2023-12-19 10:24:52 +0630

    filter|

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 (function(){
 'use strict';
 angular.module('myApp',[])
-.controller('myController',function($scope){
+.controller('myController',function($scope,$filter){
     $scope.name = 'STEVE JOBS';
     $scope.msgPhoto = 'msg1';
+    $scope.phoneCost = .34;
+    $scope.cost = 0;
     $scope.sayMessage = function(){
-        return "CREATIVITY IS JUST CONNECTING THINGS";
+        var output = $filter('uppercase')("creativity is just connecting things")
+        return output;
+    }
+    $scope.filter = function(){
+        var filt = $filter('currency')($scope.cost);
+        $scope.cost = filt;
     }
     var i = 0;
     $scope.showPhoto = function(){
