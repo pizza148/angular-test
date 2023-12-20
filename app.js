2f380a57fe1aedcdee73f33fd14e61651f16967b
Aung Kaung Khant <aungkaungkhant807@gmail.com>
2023-12-20 19:22:36 +0630

    $Watchers

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 (function(){
-'use strict';
-angular.module('myApp',[])
-.controller('myController',function($scope,$filter){
-    $scope.name = 'STEVE JOBS';
-    $scope.msgPhoto = 'msg1';
-    $scope.phoneCost = .34;
-    $scope.cost = 0;
-    $scope.sayMessage = function(){
-        var output = $filter('uppercase')("creativity is just connecting things")
-        return output;
-    }
-    $scope.filter = function(){
-        var filt = $filter('currency')($scope.cost);
-        $scope.cost = filt;
-    }
-    var i = 0;
-    $scope.showPhoto = function(){
-      $scope.msgPhoto = "msg2";
-    }
-})
+    'use strict';
+    angular.module('myApp',[])
+    .controller('myController',MyController);
+    MyController.$inject =['$scope'];
+    function MyController ($scope){
+        $scope.onceCounter = 0;
+        $scope.counter = 0;
+        $scope.showNumberOfWatchers = function(){
+            console.log('# watch counter',$scope.$$watchersCount);
+        };
+        $scope.counterOnce = function(){
+            $scope.onceCounter = 1;
+        }
+        $scope.upCounter = function(){
+            $scope.counter++;
+        }
+        $scope.$watch('onceCounter',function(newvalue,oldvalue){
+            console.log('Old Value',oldvalue);
+            console.log('new value',newvalue);
+        });
+
+        $scope.$watch('counter',function(newValue,oldValue){
+            console.log('counter old vale',oldValue);
+            console.log('counter new value',newValue);
+        })
+    };
 })();
